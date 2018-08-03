c872b42f2a2d234ea677a0532057b80abeba67c2
Ivan <ivan@izzi.com.ar>
2018-08-03 20:49:16 -0300

    Initial commit

---
+++ src/MyRAdmin/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace MyRAdmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.radioButton6 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button7 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 84);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dataGridView1.Size = new System.Drawing.Size(1263, 518);
+			this.dataGridView1.TabIndex = 0;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+			this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.PaleGreen;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(118, 609);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(127, 39);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Full Access";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(251, 608);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(102, 40);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Only View";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(359, 609);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(94, 39);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "File Transfer";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Black;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(459, 608);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(92, 40);
+			this.button4.TabIndex = 10;
+			this.button4.Text = "C M D";
+			this.button4.UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.IndianRed;
+			this.button5.FlatAppearance.BorderSize = 0;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Location = new System.Drawing.Point(557, 609);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(100, 39);
+			this.button5.TabIndex = 11;
+			this.button5.Text = "Shut Down";
+			this.button5.UseVisualStyleBackColor = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton4);
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.textBox6);
+			this.groupBox1.Controls.Add(this.radioButton6);
+			this.groupBox1.Controls.Add(this.radioButton5);
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.button6);
+			this.groupBox1.Controls.Add(this.textBox5);
+			this.groupBox1.Controls.Add(this.textBox4);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(1263, 66);
+			this.groupBox1.TabIndex = 19;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Filter";
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.AutoSize = true;
+			this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton4.Location = new System.Drawing.Point(94, 10);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(35, 17);
+			this.radioButton4.TabIndex = 33;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "ID";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox3.Location = new System.Drawing.Point(94, 33);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(169, 20);
+			this.textBox3.TabIndex = 32;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton1.Location = new System.Drawing.Point(6, 11);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(45, 17);
+			this.radioButton1.TabIndex = 31;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Tipo";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// textBox6
+			// 
+			this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox6.Location = new System.Drawing.Point(6, 34);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(82, 20);
+			this.textBox6.TabIndex = 30;
+			this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.AutoSize = true;
+			this.radioButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton6.Location = new System.Drawing.Point(841, 11);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(56, 17);
+			this.radioButton6.TabIndex = 29;
+			this.radioButton6.TabStop = true;
+			this.radioButton6.Text = "Details";
+			this.radioButton6.UseVisualStyleBackColor = true;
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.AutoSize = true;
+			this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton5.Location = new System.Drawing.Point(622, 11);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(55, 17);
+			this.radioButton5.TabIndex = 28;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "Sector";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton3.Location = new System.Drawing.Point(475, 11);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(34, 17);
+			this.radioButton3.TabIndex = 26;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "IP";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.radioButton2.Location = new System.Drawing.Point(269, 11);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(52, 17);
+			this.radioButton2.TabIndex = 25;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Name";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button6.FlatAppearance.BorderSize = 0;
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Location = new System.Drawing.Point(1060, 11);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(197, 42);
+			this.button6.TabIndex = 24;
+			this.button6.Text = "Clear";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// textBox5
+			// 
+			this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox5.Location = new System.Drawing.Point(841, 33);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(213, 20);
+			this.textBox5.TabIndex = 23;
+			this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+			// 
+			// textBox4
+			// 
+			this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox4.Location = new System.Drawing.Point(622, 33);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(213, 20);
+			this.textBox4.TabIndex = 22;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Location = new System.Drawing.Point(475, 33);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(141, 20);
+			this.textBox2.TabIndex = 20;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(269, 33);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(202, 20);
+			this.textBox1.TabIndex = 19;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button7.FlatAppearance.BorderSize = 0;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Location = new System.Drawing.Point(1175, 609);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(100, 39);
+			this.button7.TabIndex = 20;
+			this.button7.Text = "Save";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1287, 660);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "MyRAdmin";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+	}
+}
+
---
+++ src/MyRAdmin/RaHost.cs
@@ -0,0 +1,29 @@
+﻿namespace MyRAdmin {
+	public class RaHost {
+		public string ID;
+		public string Name;
+		public string IP;
+		public ushort Port;
+		public string Sector;
+		public string Tipo;
+		public string User;
+		public string Details;
+
+		public override string ToString()
+		{
+			string toReturn =
+$@"
+ID: { ID }
+Name: { Name }
+IP: { IP }
+Port: { Port }
+Sector: { Sector }
+Tipo: { Tipo }
+User: { User }
+Details: { Details }
+";
+
+			return toReturn;
+		}
+	}
+}
